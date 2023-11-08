00e2f6b40b0536b20cd2093db6d954f38a937f71
EnatanRose <vanoffgent@gmail.com>
2023-11-08 18:40:24 +0300

    Started

---
+++ src/components/app/app.js
@@ -0,0 +1,28 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css';
+
+function App(){
+    const data = [
+        {name:'John', salary:800, increase: true},
+        {name:'Alex', salary:500, increase: false},
+        {name:'Cristian', salary:15000, increase: false}
+    ]
+    return(
+        <div className="app">
+            <AppInfo/>
+            <div className='search-panel'>
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            <EmployersList data={data}/>
+            <EmployersAddForm/>
+        </div>
+    )
+}
+
+export default App;
